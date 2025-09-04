ee43b811da43e9164464f4c444d756e551c88ddb
Jamie Bryson <jamieab98@gmail.com>
2025-09-04 17:27:31 -0400

    making progess. Got the first step and half of the second step

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 // index.js
-const weatherApi = "https://api.weather.gov/alerts/active?area="
+let weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+
+
+
+function getData() {
+    const state = document.getElementById("state-input").value;
+    const target = document.getElementById("testing");
+    const displayTarget = document.getElementById("alerts-display");
+    weatherApi = `https://api.weather.gov/alerts/active?area=${state}`;
+    //displayTarget.textContent = weatherApi;
+    fetch(weatherApi)
+        .then(response => response.json())
+        .then(data => {
+            const dataTitle = data.title;
+            const alerts = data.features;
+            const numAlerts = alerts.length;
+            displayTarget.textContent = `${dataTitle}: ${numAlerts}`;
+        })
+}
+
+document.getElementById("fetch-alerts").addEventListener('click', getData);
