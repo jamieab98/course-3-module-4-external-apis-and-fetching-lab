4bdc6b113d06a17e15ee4ae8ff28ee6cbc6db8ca
Jamie Bryson <jamieab98@gmail.com>
2025-09-04 18:33:57 -0400

    finished the lab

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let weatherApi = "https://api.weather.gov/alerts/active?area="
 function getData() {
     const state = document.getElementById("state-input").value;
     const displayTarget = document.getElementById("alerts-display");
+    displayTarget.textContent = "";
     weatherApi = `https://api.weather.gov/alerts/active?area=${state}`;
     fetch(weatherApi)
         .then(response => response.json())
@@ -23,7 +24,17 @@ function getData() {
                 li.textContent = alert.properties.headline;
                 alertList.appendChild(li);
             });
+            document.getElementById("error-message").classList.add("hidden");
+            document.getElementById("error-message").textContent = "";
         })
+        .catch(errorObject => {
+            displayTarget.textContent = "";
+            console.log(errorObject.message);
+            const errorContainer = document.getElementById("error-message");
+            errorContainer.textContent = errorObject.message;
+            errorContainer.classList.remove("hidden");
+        })
+        document.getElementById("state-input").value = "";
     
 }
 
